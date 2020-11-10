6d5f2f19d7803a95003835ff9fa3bf4f8d76e271
Daniel Yankiver <dyankiver@gmail.com>
2020-11-10 21:56:24 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -145,7 +145,7 @@ def shoe_size(name)
   game_hash.each do |team, team_data|
     team_data[:players].each do |player|
       if player[:player_name] == name
-        
+        return player
     end 
   end
 end 
