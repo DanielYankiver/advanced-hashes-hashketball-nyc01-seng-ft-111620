0f9f6a1e446a7cfa8bfd891206f8603aa44dd12d
Daniel Yankiver <dyankiver@gmail.com>
2020-11-10 22:10:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -160,8 +160,8 @@ def team_colors(team)
 end 
 
 def team_names
-  game_hash.map do |team, team_|data
-  binding.pry
+  game_hash.map do |team, team_data|
+    binding.pry
     team_data[:team_name]
   end
 end 
