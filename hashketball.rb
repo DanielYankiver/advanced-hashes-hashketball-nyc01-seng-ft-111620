aacc6bbe09a8e80ecd064d7afaa6261b5ca1d371
Daniel Yankiver <dyankiver@gmail.com>
2020-11-10 22:20:00 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -168,7 +168,8 @@ end
 def player_numbers(team_name)
   game_hash.each do |team, team_data|
     if team_data[:team_name] == team_name
-      team_data.each do ||
+      team_data.each do |key, value|
+        
   
 end
   
