a7b5fd679302bfb5871dbf0d8669cdf9220708d6
Daniel Yankiver <dyankiver@gmail.com>
2020-11-10 22:47:54 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -201,7 +201,8 @@ def big_shoe_rebounds
   rebounds = 0 
   game_hash.each do |team, team_data|
     team_data[:players].each do |player|
-      if player[:shoe] > 
+      if player[:shoe] > big_shoe
+        
 end 
   
   
